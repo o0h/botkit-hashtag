b9042f07ab0d318c06e13ddfb7da6e69665dc8e1
Hideki Kinjyo <dev@o0h.in>
2018-10-14 00:26:27 +0900

    PJ setup

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+  },
+  plugins: [resolve(), commonjs(), babel()],
+}
